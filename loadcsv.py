5132383cf1bdd127052468c57c9545a1153ff89f
Aidan-Tyler <aidantyler816@gmail.com>
2026-02-07 17:33:20 +0000

    loadcsv.py

---
+++ loadcsv.py
@@ -0,0 +1,4 @@
+import pandas as pd
+df = pd.read_csv('/Workspace/Users/tyler.317@buckeyemail.osu.edu/The-Lego-Batman-2-Enjoyers-Datathon-2026-Sponsored-by-Databricks/nyc_housing_base.csv')
+
+print(df.head())
